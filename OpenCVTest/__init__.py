9bccd289edf09fa3ce75f9cf21e5db60b3b2f93e
blakedgordon <blakedgordon5@gmail.com>
2018-02-19 14:11:37 -0800

    Initial Commit

--- OpenCVTest/__init__.py
+++ OpenCVTest/__init__.py
@@ -10,18 +10,12 @@ def main():
 
         gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
 
-        thresh = cv.adaptiveThreshold(gray, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY_INV, 11, 2)
-        _, contours, _ = cv.findContours(thresh, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
-
-        for cnt in contours:
-            epsilon = 0.015 * cv.arcLength(cnt,True)
-            approx = cv.approxPolyDP(cnt,epsilon,True)
-
-            if len(approx) == 4 and cv.contourArea(cnt) > 100:
-                rect = cv.minAreaRect(cnt)
-                box = cv.boxPoints(rect)
-                box = np.int0(box)
-                cv.drawContours(frame, [box], 0, (0, 255, 0), 2)
+        circles = cv.HoughCircles(gray, cv.HOUGH_GRADIENT, 1, 10, param1=50, param2=30, minRadius=0, maxRadius=25)
+        if circles is not None :
+            circles = np.uint16(np.around(circles))
+            for i in circles[0, :]:
+                # draw the outer circle
+                cv.circle(frame, (i[0], i[1]), i[2], (0, 255, 0), 2)
 
 
         cv.imshow('OpenCV Find Squares', frame)
